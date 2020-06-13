bc62bc5e163e97a31408341a56c4745d14069687
Andrew <andrewzelenevsky@MacBook-Pro.local>
2020-06-14 00:15:12 +0300

    Final stages

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 window.addEventListener('load', ()=> {
-let long;
-let lat;
-let temperatureDegree = document.querySelector(".temperature-degree");
-let temperatureDescription = document.querySelector(".temperature-description");
-let locationTimeZone = document.querySelector(".location-timezone");
+    let long;
+    let lat;
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let locationTimeZone = document.querySelector(".location-timezone");
 
-if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
 
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
 
-        const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&
-        exclude={part}&units=metric&appid=10debf3b6ce86c4a9a95946f0f44055e`;
+            const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&
+            exclude={part}&units=metric&appid=10debf3b6ce86c4a9a95946f0f44055e`;
 
-        fetch (api)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                temperatureDegree.textContent = data.current.temp;
-                temperatureDescription.textContent = data.current.weather.shift().description;
-                locationTimeZone.textContent = data.timezone;
-                
+            fetch (api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    temperatureDegree.textContent = data.current.temp;
+                    temperatureDescription.textContent = data.current.weather.shift().description;
+                    locationTimeZone.textContent = data.timezone;
+
+                    console.log(data)
+
+                    //setIcons(temperatureDescription, document.querySelector('.icon'));
+                });
             });
-        });
-    }
+            //setIcons(document.querySelector(".icon"));
+        }
+
+        let skycons = new Skycons({"color": "white"});
+        skycons.add("icon", Skycons.RAIN);
+        skycons.play();
+
 });
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -12,14 +12,21 @@ window.addEventListener('load', function () {
     navigator.geolocation.getCurrentPosition(function (position) {
       _long = position.coords.longitude;
       lat = position.coords.latitude;
-      var api = "https://api.openweathermap.org/data/2.5/onecall?lat=".concat(lat, "&lon=").concat(_long, "&\n        exclude={part}&units=metric&appid=10debf3b6ce86c4a9a95946f0f44055e");
+      var api = "https://api.openweathermap.org/data/2.5/onecall?lat=".concat(lat, "&lon=").concat(_long, "&\n            exclude={part}&units=metric&appid=10debf3b6ce86c4a9a95946f0f44055e");
       fetch(api).then(function (response) {
         return response.json();
       }).then(function (data) {
         temperatureDegree.textContent = data.current.temp;
         temperatureDescription.textContent = data.current.weather.shift().description;
         locationTimeZone.textContent = data.timezone;
+        console.log(data); //setIcons(temperatureDescription, document.querySelector('.icon'));
       });
-    });
+    }); //setIcons(document.querySelector(".icon"));
   }
+
+  var skycons = new Skycons({
+    "color": "white"
+  });
+  skycons.add("icon", Skycons.RAIN);
+  skycons.play();
 });
